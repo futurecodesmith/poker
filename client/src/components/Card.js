2a1eb9d5f131bffd1d114a1b5d720211472a0f18
Matthew Laufer <matthew.j.laufer@gmail.com>
2017-03-31 14:20:04 -0700

    adding react components - not finished

---
+++ client/src/components/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Card = (props) => {
+
+  render() {
+    let cardBackgroundUrl = this.props.hidden ? 'url(img/hidden.png)' : 'url(img/' + this.props.face + '.png)';
+    const cardStyle = { backgroundImage: cardBackgroundUrl };
+
+    return (
+      <div className='card' style={ cardStyle }/>
+    );
+  }
+
+}
+
+export default Card;
