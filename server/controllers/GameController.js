1cb827352b46061c2de1e8b9458f3a5c7dd2b96e
Ryan Fowlkes Smith <ryan.fowlkes@gmail.com>
2017-03-30 19:47:04 -0700

    makin it work

---
+++ server/controllers/GameController.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Game = require('../models/Game.js');
+
+
+mongoose.connect('mongodb://localhost/poker-game');
+
+const GameController = {
+  createGame: (req, res, next) => {
+    User.create(req.body, (err, doc) => {
+      if (err) throw err;
+      else next();
+    })
+  }
+}
+
+module.exports = GameController;
---
+++ server/controllers/UserController.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const User = require('../models/User.js');
+
+
+mongoose.connect('mongodb://localhost/poker-game');
+
+const UserController = {
+  createUser: (req, res, next) => {
+    User.create(req.body, (err, doc) => {
+      if (err) throw err;
+      else next();
+    })
+  }
+}
+
+module.exports = UserController;
