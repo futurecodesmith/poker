49244c697254157de1299121a5990b5f9fd44714
Ryan Fowlkes Smith <ryan.fowlkes@gmail.com>
2017-03-31 14:17:46 -0700

    commiting before pulling

--- Login.js
+++ Login.js
@@ -1,20 +1,13 @@
 import React, { Component } from 'react';
 
-function handleSubmit() {
-
-}
-
-
 const Login = (props) => {
-
-
   return (
     <div id="login">
       <form>
         <input type="text" placeholder="Username...">
         <input type="password" placeholder="Password...">
       </form>
-      <a>Create Account</a>
+      <button onClick={props.handleClick}>Create Account</button>
     </div>
   );
 }
--- NewUser.js
+++ NewUser.js
@@ -1,19 +1,5 @@
 import React, { Component } from 'react';
-const $ = require('jQuery');
 
-// function handleSubmit() {
-//   const userData = JSON.stringify({
-//     username: document.getElementById('username').value,
-//     password: document.getElementById('password').value
-//   })
-//   const postReq = $.ajax({
-//     type: 'POST',
-//     url: '/user',
-//     data: userData,
-//     contentType: 'application/json',
-//   });
-//   postReq.then()
-// }
 
 
 const NewUser = (props) => {
@@ -22,9 +8,9 @@ const NewUser = (props) => {
   return (
     <div id="newUser">
       <h3>New Account</h3>
-      <form>
-        <input id='username' type="text" placeholder="Username..." />
-        <input id='password' type="password" placeholder="Password..." />
+      <form method="POST" action="/user">
+        <input name='username' type="text" placeholder="Username..." />
+        <input name='password' type="password" placeholder="Password..." />
         <input type="submit" value="Create Account" />
       </form>
     </div>
